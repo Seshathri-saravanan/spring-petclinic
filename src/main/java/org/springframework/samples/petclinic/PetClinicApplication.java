b16bd34e8b0b8c1863797bcb40c23f396edd400d
Seshathri S <67517564+Seshathri-saravanan@users.noreply.github.com>
2024-05-20 15:38:10 +0530

    Update PetClinicApplication.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-
 package org.springframework.samples.petclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
